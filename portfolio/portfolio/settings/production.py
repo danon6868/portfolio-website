f4bac3d8dfe41f92e8f6319f8e0879bb03cef058
daniil_litvinov <danil.litvinov@bostongene.com>
2022-10-09 23:19:46 +0400

    minor

--- portfolio/portfolio/settings/production.py
+++ portfolio/portfolio/settings/production.py
@@ -1,4 +1,12 @@
 from .base import *
+import dj_database_url
+
+
+DATABASES["default"] = dj_database_url.config()
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+# Allow all host headers
+ALLOWED_HOSTS = ["*"]
 
 DEBUG = False
 
